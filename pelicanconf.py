d7b46af31b1c4fe3402c03a37296c1fa669de9a6
Takashi Odaira <odaira@insightech.co.jp>
2020-07-13 14:24:38 +0900

    設定ファイル修正

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+     ('GitHub', 'https://github.com/odeilly'),
+)
 
 DEFAULT_PAGINATION = 10
 
@@ -51,10 +49,6 @@ BOOTSTRAP_THEME = 'cerulean'
 PYGMENTS_STYLE = 'emacs'
 
 DISPLAY_TAGS_ON_SIDEBAR = True
-SOCIAL = (
-     ('GitHub', 'https://github.com/odeilly'),
-)
-LINKS = ()
 
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 PLUGIN_PATHS = ['../pelican-plugins']
