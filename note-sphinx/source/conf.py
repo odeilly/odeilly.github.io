af56adf17a7c3ed3de625af9be4f7b75df8751be
Takashi Odaira <odaira@insightech.co.jp>
2021-08-23 13:44:09 +0900

    作り直し

--- note-sphinx/source/conf.py
+++ note-sphinx/source/conf.py
@@ -21,9 +21,6 @@ project = "Odeilly's Note"
 copyright = '2021, Odeilly'
 author = 'Odeilly'
 
-# The full version, including alpha/beta/rc tags
-release = '1'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -55,13 +52,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_css_files = [
-    'odeilly.css'
-]
