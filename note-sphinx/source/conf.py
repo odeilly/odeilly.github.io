99f422b7feccec100be901bd1ba77e80643b9f28
Takashi Odaira <odaira@insightech.co.jp>
2021-08-23 17:04:52 +0900

    ReadTheDoc の設定

--- note-sphinx/source/conf.py
+++ note-sphinx/source/conf.py
@@ -54,6 +54,10 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'prev_next_buttons_location': 'both',
+    'style_external_links': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
